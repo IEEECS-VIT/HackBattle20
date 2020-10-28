788ad80174c8ee80b71e2c4bf628b06fb2b0ae70
[supraditya] <[aditya300100@gmail.com]>
2020-10-28 11:40:26 +0530

    Basic styling to each view section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     var myFullpage = new fullpage('#fullpage', {
-        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+        sectionsColor: ['#848322', '#FFFFFF', '#7BAABE', 'whitesmoke', '#ccddff']
     });
 });
