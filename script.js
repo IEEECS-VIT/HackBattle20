5ec8a37c62b351ba87fa70d98a03bdecc07da70a
[supraditya] <[aditya300100@gmail.com]>
2020-10-28 17:40:27 +0530

    About view ready with placeholder image

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     var myFullpage = new fullpage('#fullpage', {
-        sectionsColor: ['#848322', '#FFFFFF', '#7BAABE', 'whitesmoke', '#ccddff']
+        sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF']
     });
 });
