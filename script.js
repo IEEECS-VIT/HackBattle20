732adb7743968d3778a2ece5bc0667ab5687255d
[supraditya] <[aditya300100@gmail.com]>
2020-11-02 00:49:59 +0530

    Added tracks elements with styling

--- script.js
+++ script.js
@@ -16,20 +16,22 @@ $(document).ready(function() {
         $('#fp-nav ul li:nth-child(3) a').click();
     });
     $('#prizes-link').click(function(){
-        $('#fp-nav ul li:nth-child(4) a').click();
-    });
-    $('#schedule-link').click(function(){
         $('#fp-nav ul li:nth-child(5) a').click();
     });
-    $('#faq-link').click(function(){
+    $('#schedule-link').click(function(){
         $('#fp-nav ul li:nth-child(6) a').click();
     });
-    $('#sponsors-link').click(function(){
+    $('#faq-link').click(function(){
         $('#fp-nav ul li:nth-child(7) a').click();
+    });
+    $('#sponsors-link').click(function(){
+        $('#fp-nav ul li:nth-child(8) a').click();
     });
     
 });
 $(document).ready(function(){
+    $("#day2").hide();
+    $("#day3").hide();
     $("#d1").click(function(){
         $("#day1").fadeIn();
         $("#day2").fadeOut();
