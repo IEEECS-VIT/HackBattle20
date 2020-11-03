2bdd4b5f923012c6051dd05a9e85514ecdc78a72
Prerna Sultania <prernasultania2000@gmail.com>
2020-11-03 19:32:27 +0530

    hamburger

--- script.js
+++ script.js
@@ -79,3 +79,93 @@ $(document).ready(function(){
     });
 
 });
+var burgerMenu = function() {
+
+    $('body').on('click', '.js-fh5co-nav-toggle', function(event){
+
+        event.preventDefault();
+
+        if ( $('#ftco-nav').is(':visible') ) {
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');	
+        }
+
+        
+        
+    });
+
+};
+burgerMenu();
+
+
+var onePageClick = function() {
+
+
+    $(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    var href = $.attr(this, 'href');
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 70
+    }, 500, function() {
+        // window.location.hash = href;
+    });
+    });
+
+};
+
+onePageClick();
+
+
+var carousel = function() {
+    $('.home-slider').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:0,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav:false,
+    autoplayHoverPause: false,
+    items: 1,
+    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:1
+      },
+      1000:{
+        items:1
+      }
+    }
+    });
+};
+carousel();
+
+$('nav .dropdown').hover(function(){
+    var $this = $(this);
+    // 	 timer;
+    // clearTimeout(timer);
+    $this.addClass('show');
+    $this.find('> a').attr('aria-expanded', true);
+    // $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+    $this.find('.dropdown-menu').addClass('show');
+}, function(){
+    var $this = $(this);
+        // timer;
+    // timer = setTimeout(function(){
+        $this.removeClass('show');
+        $this.find('> a').attr('aria-expanded', false);
+        // $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+        $this.find('.dropdown-menu').removeClass('show');
+    // }, 100);
+});
+
+
+$('#dropdown04').on('show.bs.dropdown', function () {
+  console.log('show');
+});
+
