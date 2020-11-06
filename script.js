acff5c544f419ca24b20b144666003f8bc1459c9
[supraditya] <[aditya300100@gmail.com]>
2020-11-07 00:38:39 +0530

    Fix for scrolling ahead of contact us in mobile view

--- script.js
+++ script.js
@@ -163,6 +163,7 @@ $(document).ready(function () {
   $('.closebtn').click(function(){
     $('.openbtn').removeClass('hide');
   });
+
 });
 // var burgerMenu = function() {
 
@@ -252,4 +253,9 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-} 
+}
+document.querySelector('#contact').addEventListener("wheel", function(){
+  document.querySelector("#fp-nav ul li:nth-child(1) a").click();
+});
+
+// document.getElementById("contact-wrapper-mobile").addEventListener("wheel", loopBack); 
